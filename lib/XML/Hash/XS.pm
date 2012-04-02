e8fc28e14f324952671d8042eeafb6c070ecb00d
Yuriy <yoreek@yahoo.com>
2012-04-02 11:52:24 +0300

    Updated version to 0.05_03

--- lib/XML/Hash/XS.pm
+++ lib/XML/Hash/XS.pm
@@ -7,7 +7,7 @@ use warnings;
 use base 'Exporter';
 our @EXPORT_OK = our @EXPORT = qw( hash2xml );
 
-our $VERSION = '0.05_02';
+our $VERSION = '0.05_03';
 
 require XSLoader;
 XSLoader::load('XML::Hash::XS', $VERSION);
