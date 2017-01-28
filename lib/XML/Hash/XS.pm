e82744353c144450413bb23efe126f92fafac97f
Yuriy Ustushenko <yoreek@yahoo.com>
2017-01-28 19:24:22 +0300

    - Fix bug occurs while using multiple job compile

--- lib/XML/Hash/XS.pm
+++ lib/XML/Hash/XS.pm
@@ -7,7 +7,7 @@ use vars qw($VERSION @EXPORT @EXPORT_OK);
 use base 'Exporter';
 @EXPORT_OK = @EXPORT = qw( hash2xml xml2hash );
 
-$VERSION = '0.45';
+$VERSION = '0.46';
 
 require XSLoader;
 XSLoader::load('XML::Hash::XS', $VERSION);
